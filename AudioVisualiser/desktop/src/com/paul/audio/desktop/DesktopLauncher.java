a840937fa55ae2699a355fc8ad67d19e0d292f7e
Paul <paul@lancasterzone.com>
2016-12-11 19:57:40 +0000

    Generated the LIBGDX base files. Nothing has been done with them yet until program works using basic java

---
+++ AudioVisualiser/desktop/src/com/paul/audio/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.paul.audio.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.paul.audio.AudioVisualise;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AudioVisualise(), config);
+	}
+}
---
+++ AudioVisualiser/html/src/com/paul/audio/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.paul.audio.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.paul.audio.AudioVisualise;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new AudioVisualise();
+        }
+}
