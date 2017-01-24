a9ed84db12f46dfd1b758011340e0883bd30a893
Paul Lancaster <paul@lancasterzone.com>
2017-01-24 15:50:08 +0000

    Circle Effect

--- src/effects/CircularEffect.java
+++ src/effects/CircularEffect.java
@@ -1,14 +1,22 @@
 package effects;
 
 import audio.file.AudioFile;
+import calculate.FreqCalculator;
 
 import java.awt.*;
+import java.awt.List;
+import java.util.*;
 
 /**
  * Created by paul on 23/01/17.
  */
 public class CircularEffect extends VisualEffect{
     int PADDING = 10;
+    int BASE_DIAMETER = 16;
+    private long dTSinceLastFrequency = 0;
+    private long minNanoPerFrequencyUpdate;
+
+
     /**
      * Loads the visual effect using details from the given LoadedFile and encapsulates that file
      *
@@ -20,13 +28,21 @@ public class CircularEffect extends VisualEffect{
 
     @Override
     protected void drawEffect(Graphics2D g2d, int width, int height, long deltaT) {
-        int x = (int)(width /2.0);
-        int y = (int)(height/2.0);
-        int diameter = (int)(width/2.0) - PADDING;
-        if (diameter <= 0){
-            diameter = 1;
+
+        if (dTSinceLastFrequency >= minNanoPerFrequencyUpdate){
+            int x = (int)(width /2.0);
+            int y = (int)(height/2.0);
+            int diameter = calcDiameter();
+            g2d.fillOval(x,y,diameter,diameter);
+            dTSinceLastFrequency = 0;
+        }else{
+            dTSinceLastFrequency = dTSinceLastFrequency + deltaT;
         }
-        g2d.fill(x,y,diameter,diameter);
+    }
+
+    private int calcDiameter(int[] chunk, int sampleRate) {
+        FreqCalculator.getPrimaryFreqOfChunk(chunk,sampleRate);
+        return BASE_DIAMETER;
     }
 
     @Override
@@ -42,5 +58,17 @@ public class CircularEffect extends VisualEffect{
     @Override
     public void finish() {
 
+    }
+
+    public void collectionsExamples(){
+        ArrayList<Integer> arrayList;
+        List simpleList = new List();
+        Vector<Integer> vector; // Old array from early java
+        LinkedList linkedList;
+
+        Set<Integer> mySet = new TreeSet<>();
+
+        Map<String,Integer> myMap = new HashMap<>();
+        myMap.put("Key",20);
     }
 }
