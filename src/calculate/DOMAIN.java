73ce6b27f54a7982a66d6a25e2ca219dcd413a01
Paul <paul@lancasterzone.com>
2017-01-29 19:56:47 +0000

    Cleaned calculate package

--- src/calculate/DOMAIN.java
+++
@@ -1,6 +0,0 @@
-package calculate;
-
-// Enum to represent the 3 types (2 domains + the magnitude of thoses domains) of values that represent a complex number
-public enum DOMAIN{
-    IMAGINARY, REAL, MAGNITUDE
-}
